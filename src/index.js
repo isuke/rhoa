0772f581a2b6b13929f1dfd9a866ed079566e3b5
isuke <isuke770@gmail.com>
2017-12-02 12:27:42 +0900

    Add FlexibleImage to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import RhoaSmartLink from '@components/SmartLink';
+import RhoaFlexibleImage from '@components/FlexibleImage';
 
 export const Version = '0.0.3';
 
 export const SmartLink = RhoaSmartLink;
+export const FlexibleImage = RhoaFlexibleImage;
--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,6 @@
 module.exports = {
   install: function(Vue, options) {
     Vue.component('smart-link', require('@components/SmartLink'));
+    Vue.component('flexible-image', require('@components/FlexibleImage'));
   }
 };
