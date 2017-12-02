3b7dea6345d6e63e93690083d2aaeb00cee575f9
isuke <isuke770@gmail.com>
2017-12-02 21:50:00 +0900

    Upgrade to v0.0.6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import RhoaSmartLink from '@components/SmartLink';
 import RhoaFlexibleImage from '@components/FlexibleImage';
 
-export const VERSION = '0.0.5';
+export const VERSION = '0.0.6';
 
 export const SmartLink = RhoaSmartLink;
 export const FlexibleImage = RhoaFlexibleImage;
